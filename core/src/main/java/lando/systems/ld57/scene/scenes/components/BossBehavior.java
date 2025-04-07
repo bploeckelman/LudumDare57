e6042cfe1f3610309aa0a4947e72487099ac87ed
Brian R <bandrewrossman@gmail.com>
2025-04-07 15:40:26 -0500

    basic flying boss

---
+++ core/src/main/java/lando/systems/ld57/scene/scenes/components/BossBehavior.java
@@ -0,0 +1,125 @@
+package lando.systems.ld57.scene.scenes.components;
+
+import com.badlogic.gdx.math.MathUtils;
+import com.badlogic.gdx.math.Vector2;
+import lando.systems.ld57.assets.Anims;
+import lando.systems.ld57.particles.effects.BloodSplatEffect;
+import lando.systems.ld57.particles.effects.DirtEffect;
+import lando.systems.ld57.particles.effects.ParticleEffect;
+import lando.systems.ld57.scene.components.Animator;
+import lando.systems.ld57.scene.components.Collider;
+import lando.systems.ld57.scene.components.DebugRender;
+import lando.systems.ld57.scene.components.Health;
+import lando.systems.ld57.scene.components.Mover;
+import lando.systems.ld57.scene.components.ParticleEmitter;
+import lando.systems.ld57.scene.components.Position;
+import lando.systems.ld57.scene.framework.Entity;
+import lando.systems.ld57.scene.scenes.PlayerBehavior;
+import lando.systems.ld57.utils.Util;
+
+public class BossBehavior extends EnemyBehavior {
+
+    private static final float ATTACK_COOLDOWN = 2f;
+    private static final float ATTACK_DURATION = 0.8f;
+
+    private float timer = ATTACK_COOLDOWN;
+    private boolean attacking = false;
+
+
+    Entity body;
+    Entity bowser;
+    Entity gannon;
+    Entity wily;
+    Entity dracula;
+
+    private final Entity[] parts;
+
+    private int facing = 1;
+    private boolean movingRight = false;
+    private float speed = 20f;
+    private float totalMovement = 0f;
+    private float moveRadius = 80f;
+    private final Vector2 startPos;
+    private float widthOffset;
+
+    public BossBehavior(Entity entity, Entity body, Entity bowser, Entity gannon, Entity wily, Entity dracula) {
+        super(entity);
+
+        var bossPos = entity.get(Position.class);
+        startPos = new Vector2(bossPos.x(), bossPos.y());
+
+        this.body = body;
+        this.bowser = bowser;
+        this.gannon = gannon;
+        this.wily = wily;
+        this.dracula = dracula;
+
+        this.parts = new Entity[] { body, bowser, gannon, wily, dracula };
+
+        this.widthOffset = this.body.get(Animator.class).keyframe.getRegionWidth() / 4f;
+    }
+
+    @Override
+    public void update(float dt) {
+        facePlayer();
+        move(dt);
+    }
+
+    private void facePlayer() {
+        var player = entity.scene.player;
+        if (player != null) {
+            var thisPos = entity.get(Position.class);
+            var playerPos = player.get(Position.class);
+            // left
+            if (facing < 0) {
+                facing = (playerPos.x() > thisPos.x() + widthOffset) ? 1 : -1;
+            } else {
+                facing = (playerPos.x() < thisPos.x() - widthOffset) ? -1 : 1;
+            }
+        }
+    }
+
+    private void move(float dt) {
+         totalMovement += dt;
+         float dx = this.speed * dt;
+         if (!movingRight) {
+             dx *= -1;
+         }
+
+         var floatValue = MathUtils.sin(totalMovement * 3);
+
+         var position = entity.get(Position.class);
+         float x = position.x() + dx;
+         if (x < startPos.x - moveRadius) {
+             movingRight = true;
+         } else if (x > startPos.x + moveRadius) {
+             movingRight = false;
+         }
+
+         float y = position.y() + floatValue * 3;
+         position.set(x, position.y());
+
+         for (var entity : parts) {
+             entity.get(Animator.class).facing = facing;
+             entity.get(Position.class).set(x, y);
+         }
+    }
+
+    @Override
+    public void attack() {
+//        var animator = entity.get(Animator.class);
+//        animator.play(Anims.Type.SKELETON_ATTACK);
+//        prevFacing = animator.facing;
+//
+//        var mover = entity.get(Mover.class);
+//        prevVelocity.set(mover.velocity);
+//        mover.stopX();
+//
+//        var position = entity.get(Position.class);
+//        var emitter = entity.get(ParticleEmitter.class);
+//        emitter.spawnParticle(ParticleEffect.Type.DIRT,
+//            new DirtEffect.Params(position.x(), position.y() + animator.size.y / 2f));
+//
+//        throwBone();
+    }
+}
--- core/src/main/java/lando/systems/ld57/world/BossFactory.java
+++ core/src/main/java/lando/systems/ld57/world/BossFactory.java
@@ -1,67 +1,44 @@
 package lando.systems.ld57.world;
 
-import com.badlogic.gdx.math.MathUtils;
 import lando.systems.ld57.assets.Anims;
 import lando.systems.ld57.scene.Scene;
 import lando.systems.ld57.scene.components.*;
 import lando.systems.ld57.scene.framework.Entity;
+import lando.systems.ld57.scene.scenes.components.BossBehavior;
 import lando.systems.ld57.screens.BaseScreen;
 
 public class BossFactory {
 
     public static Entity createBoss(Scene<? extends BaseScreen> scene, float x, float y) {
-        return createBoss(scene, x, y, false);
-    }
-
-    public static Entity createBoss(Scene<? extends BaseScreen> scene, float x, float y, boolean shrink) {
         var entity = scene.createEntity();
         new Position(entity, x, y);
         new Health(entity, 3000f);
 
-        addBossPart(entity, Anims.Type.BOSS_BODY, 15, 95, 30, shrink);
-        addBossPart(entity, Anims.Type.BOSS_NECK_HEAD_BOWSER, 70, 127, 10, shrink);
-        addBossPart(entity, Anims.Type.BOSS_NECK_HEAD_WILY, 45, 150, 10, shrink);
-        addBossPart(entity, Anims.Type.BOSS_NECK_HEAD_GANON, 10, 160, 10, shrink);
-        addBossPart(entity, Anims.Type.BOSS_NECK_HEAD_DRACULA, -25, 140, 10, shrink);
+        var body = addBossPart(entity, Anims.Type.BOSS_BODY, 15, 95, 30);
+        var bowser = addBossPart(entity, Anims.Type.BOSS_NECK_HEAD_BOWSER, 70, 127, 10);
+        var wily = addBossPart(entity, Anims.Type.BOSS_NECK_HEAD_WILY, 45, 150, 10);
+        var gannon = addBossPart(entity, Anims.Type.BOSS_NECK_HEAD_GANON, 10, 160, 10);
+        var dracula = addBossPart(entity, Anims.Type.BOSS_NECK_HEAD_DRACULA, -25, 140, 10);
+
+        new BossBehavior(entity, body, bowser, wily, gannon, dracula);
 
         return entity;
     }
 
-    private static void addBossPart(Entity entity, Anims.Type type, float x, float y, float radius, boolean shrink) {
+    private static Entity addBossPart(Entity entity, Anims.Type type, float x, float y, float radius) {
         var part = entity.scene.createEntity();
         var pos = entity.get(Position.class);
         new Position(part, pos.x(), pos.y());
 
         var scale = 1f;
-        if (shrink) {
-            scale = 0.2f;
-        }
 
         var animator = new Animator(part, type);
         animator.origin.set(45, 1);
         animator.defaultScale.scl(scale);
 
-        var collider = Collider.makeCirc(part, Collider.Mask.enemy, x * scale, y * scale, radius * scale);
+        Collider.makeCirc(part, Collider.Mask.enemy, x * scale, y * scale, radius * scale);
         DebugRender.makeForShapes(part, DebugRender.DRAW_POSITION_AND_COLLIDER);
 
-
-//        var mover = new Mover(part, collider);
-//        mover.velocity.setToRandomDirection().scl(2);
-//        mover.addCollidesWith(Collider.Mask.solid);
-//        mover.setOnHit((params) -> {
-//            // invert speed on the hit axis and add some squash/stretch
-//            switch (params.direction) {
-//                case LEFT:
-//                case RIGHT: {
-//                    mover.invertX();
-//                    //image.scale.set(0.66f, 1.33f);
-//                } break;
-//                case UP:
-//                case DOWN: {
-//                    mover.invertY();
-//                    //image.scale.set(1.33f, 0.66f);
-//                } break;
-//            }
-//        });
+        return part;
     }
 }
